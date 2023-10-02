16def867031e44e8230d431b9edc45bf545531bc
GABRIEL HENRIQUE COSTA DA SILVA <gabriel.07907@aluno.pr.senac.br>
2023-10-02 17:06:05 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/sistemavendas/SistemaVendas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemavendas;
+
+/**
+ *
+ * @author gabriel.07907
+ */
+public class SistemaVendas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
